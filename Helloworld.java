c255ff1a8d6773c858c9998d32aabb5a55761f58
Bhaneshwar Kumar <55010022+bhaneshwar@users.noreply.github.com>
2021-11-11 13:16:21 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,6 +1,6 @@
 public class Helloworld{
 
 public static void main(String arg[]){
-System.out.println(" hello world test on mac " );
+System.out.println(" hello world test on mac 11-Nov21" );
 }
 }
